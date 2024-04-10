d1cda5c1a68492948bda49a9df974464092e6346
dz1dzor <kbismarkkwaku@gmail.com>
2024-04-10 16:22:51 +0000

    changed folder name for the archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,10 +11,12 @@ from fabric.api import local
 def do_pack():
     """compress the web_static folder"""
     try:
+        timestamp = time.strftime("%Y%m%d%H%M%S")
         local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
-    except Exception:
+        archived_folder = f"versions/web_static_{timestamp}.tgz"
+        local(f"tar -cvzf {archived_folder} web_static/")
+
+        return archived_folder
+    except Exception as e:
+        print(f"Error: {e}")
         return None
